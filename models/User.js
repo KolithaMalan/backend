deb4a815883e9e4ddd70eb22a7585dc57d4cbf6e
KolithaMalan <koliyxmalan121@gmail.com>
2026-04-27 11:41:07 +0530

    Add reset password fields to User model

--- models/User.js
+++ models/User.js
@@ -63,6 +63,14 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         default: false
     },
+    resetPasswordToken: {
+        type: String,
+        default: undefined
+    },
+    resetPasswordExpire: {
+        type: Date,
+        default: undefined
+    },
     createdAt: {
         type: Date,
         default: Date.now
